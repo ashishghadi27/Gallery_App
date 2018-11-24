65c7d1316472751ca7d5f9a953634ca83d84fcf7
Ashish Ghadigaonkar <ashishghadi27@gmail.com>
2018-11-24 17:41:06 +0530

    5:40

--- app/src/main/java/com/kaamchore/rootdevs/ashish/atg/Main2Activity.java
+++ app/src/main/java/com/kaamchore/rootdevs/ashish/atg/Main2Activity.java
@@ -2,6 +2,7 @@ package com.kaamchore.rootdevs.ashish.atg;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -14,7 +15,11 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -25,7 +30,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private NavigationView navigationView;
@@ -33,33 +38,58 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private SwipeRefreshLayout swipeRefreshLayout;
     private ImageAdapter adapter;
+    private EditText editText;
     private android.support.v7.app.ActionBar toolbar;
     List<Photo> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mDrawerLayout = findViewById(R.id.navi);
+        setContentView(R.layout.activity_main2);
         navigationView = findViewById(R.id.nav_view);
+        mDrawerLayout = findViewById(R.id.navi);
         navi_drawer = new Navi_drawer();
         navi_drawer.nav(mDrawerLayout, navigationView);
+
         swipeRefreshLayout = findViewById(R.id.swipe);
         toolbar = getSupportActionBar();
-        toolbar.setTitle("Recents");
-        
+        toolbar.setTitle("Search");
+        editText = (EditText) findViewById(R.id.searchfield);
+
+        initViews();
+
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+
+
+                if (actionId == EditorInfo.IME_ACTION_GO && (!editText.getText().toString().equals(""))) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("searchkey",MODE_PRIVATE);
+                    final SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("word", editText.getText().toString());
+                    editor.apply();
+                    if(isNetworkAvailable())
+                        loadJSON();
+                    else Toast.makeText(Main2Activity.this,"Check Connection", Toast.LENGTH_SHORT).show();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 photos.clear();
                 if(isNetworkAvailable())
-                loadJSON();
-                else Toast.makeText(MainActivity.this,"Check Connection", Toast.LENGTH_SHORT).show();
+                    loadJSON();
+                else Toast.makeText(Main2Activity.this,"Check Connection", Toast.LENGTH_SHORT).show();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
-        initViews();
     }
 
 
@@ -68,20 +98,19 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 4);
         recyclerView.setLayoutManager(layoutManager);
-        if(isNetworkAvailable())
-            loadJSON();
-        else Toast.makeText(MainActivity.this,"Check Connection", Toast.LENGTH_SHORT).show();
+
     }
     private void loadJSON(){
         final ProgressDialog dialog = new ProgressDialog(this);
         dialog.setMessage("Loading");
         dialog.show();
+        SharedPreferences sharedPreferences = getSharedPreferences("searchkey",MODE_PRIVATE);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.flickr.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-         RequestInterface request = retrofit.create(RequestInterface.class);
-        Call<Imagelist> call = request.getJSON();
+        RequestInterface request = retrofit.create(RequestInterface.class);
+        Call<Imagelist> call = request.getSearch(sharedPreferences.getString("word", ""));
         call.enqueue(new Callback<Imagelist>(){
             @Override
             public void onResponse(@NonNull Call<Imagelist> call, @NonNull Response<Imagelist> response) {
--- app/src/main/java/com/kaamchore/rootdevs/ashish/atg/RequestInterface.java
+++ app/src/main/java/com/kaamchore/rootdevs/ashish/atg/RequestInterface.java
@@ -1,14 +1,16 @@
 package com.kaamchore.rootdevs.ashish.atg;
 
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface RequestInterface {
 
     @GET("services/rest/?method=flickr.photos.getRecent&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s")
     Call<Imagelist> getJSON();
+
+
+    @GET("services/rest/?method=flickr.photos.search&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s")
+    Call<Imagelist> getSearch(@Query("text")String search);
 }
